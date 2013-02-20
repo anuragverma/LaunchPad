b9d376770eec1b76ffd2e4b1f38ec04700ad201f
anurag verma <anuragverma29@gmail.com>
2013-02-20 15:59:14 +0530

    added a new class Puzzles.cs to add code for a few coding problems.
    code for determining whether a string contains duplicate characters;
    code for reversing a string;
    code to remove duplicate characters from a string.

---
+++ LaunchPad/Puzzles.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaunchPad
+{
+    public class Puzzles
+    {
+        #region Implement an algorithm to determine if a string has all unique characters. What if you can not use additional data structures?
+        public static bool IsUniqueChars(String inputString)
+        {
+            bool[] charset = new bool[256];
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                int char_value = inputString.ElementAt(i);
+                if (charset[char_value])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        #endregion
+
+        #region Write code to reverse a String.
+        public static string ReverseString(String inputString)
+        {
+            if (inputString.Length > 0)
+            {
+                StringBuilder sb = new StringBuilder(inputString.Length);
+                for (int i = inputString.Length - 1; i >= 0; i--)
+                {
+                    sb.Append(inputString.ElementAt(i));
+                }
+                return sb.ToString();
+            }
+            else
+            {
+                return "";
+            }
+            
+        }
+        #endregion
+
+        #region Design an algorithm and write code to remove the duplicate characters in a string without using any additional buffer. NOTE: One or two additional variables are fine. An extra copy of the array is not.
+        public static void RemoveDuplicateChars(char[] inputString)
+        {
+            if (inputString == null)
+            {
+                return;
+            }
+            int length = inputString.Length;
+            if (length < 2)
+            {
+                return;
+            }
+            int tail = 1;
+            for (int i = 1; i < inputString.Length; i++)
+            {
+                int j;
+                for ( j = 0; j < tail; j++)
+                {
+                    if (inputString[i] == inputString[j])
+                    {
+                        break;
+                    }
+                }
+                if (j == tail)
+                {
+                    inputString[tail] = inputString[i];
+                    tail++;
+                }
+            }
+            for (int k = tail; k < inputString.Length; k++)
+            {
+                inputString[k] = '\0';
+            }
+        }
+        #endregion
+    }
+}
---
+++ LaunchPadTest/PuzzleTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LaunchPadTest
+{
+    [TestClass]
+    public class PuzzleTest
+    {
+        [TestMethod]
+        public void IsUniqueCharsTest()
+        {
+            Assert.AreEqual(true, LaunchPad.Puzzles.IsUniqueChars("fdghre"));
+        }
+
+        [TestMethod]
+        public void ReverseStringTest1()
+        {
+            Assert.AreEqual("london", LaunchPad.Puzzles.ReverseString("nodnol"));
+        }
+
+        [TestMethod]
+        public void ReverseStringTest2()
+        {
+            Assert.AreEqual("1245fd", LaunchPad.Puzzles.ReverseString("df5421"));
+        }
+
+        [TestMethod]
+        public void RemoveDuplicateCharsTest1()
+        {
+            char[] inArray = {'d', 'g', 'f', 'd', 'g', 'h'};
+            LaunchPad.Puzzles.RemoveDuplicateChars(inArray);
+            char[] outArray = { 'd', 'g', 'f', 'h', '\0', '\0'};
+            bool flag = Enumerable.SequenceEqual(inArray, outArray);
+            Assert.AreEqual(true, flag);
+        }
+    }
+}
