99e965cd313d776e92180ac5096543626d61d00a
anurag verma <anuragverma29@gmail.com>
2013-02-21 17:08:43 +0530

    added code for rotating 2d matrix
    code to rotate a NxN array

--- LaunchPad/Puzzles.cs
+++ LaunchPad/Puzzles.cs
@@ -133,7 +133,6 @@ namespace LaunchPad
             }
             newLength = length + spaceCount * 2;
             char[] newStr = new char[newLength];
-            //newStr[newLength] = '\0';
             for (i = length - 1; i >= 0; i--)
             {
                 if (str[i] == ' ')
@@ -150,6 +149,38 @@ namespace LaunchPad
                 }
             }
             return newStr;
+        }
+        #endregion
+
+        #region Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+        /*
+         * The rotation can be performed in layers, 
+         * where you perform a cyclic swap on the edges on each layer. 
+         * In the first for loop, we rotate the first layer (outermost edges). 
+         * We rotate the edges by doing a four-way swap first on the corners, 
+         * then on the element clockwise from the edges, then on the element three steps away.
+         */
+        public static void Rotate2DMatrix(int[,] matrix, int n)
+        {
+            for (int layer = 0; layer < n / 2; layer++)
+            {
+                int first = layer;
+                int last = n - 1 - layer;
+                for (int i = first; i < last; i++)
+                {
+                    int offset = i - first;
+                    //save top
+                    int top = matrix[first,i];
+                    //left goes to top
+                    matrix[first,i] = matrix[last - offset,first];
+                    //bottom goes to left
+                    matrix[last - offset,first] = matrix[last,last - offset];
+                    //right goes to bottom
+                    matrix[last,last - offset] = matrix[i,last];
+                    //top goes to right
+                    matrix[i,last] = top;
+                }
+            }
         }
         #endregion
     }
--- LaunchPadTest/PuzzleTest.cs
+++ LaunchPadTest/PuzzleTest.cs
@@ -50,5 +50,14 @@ namespace LaunchPadTest
             bool flag = Enumerable.SequenceEqual(result, outstr);
             Assert.AreEqual(true, flag);
         }
+
+        [TestMethod]
+        public void Rotate2DMatrixTest()
+        {
+            int[,] input = new int[5,5] { {1,2,3,4,9}, {5,6,198,7,8}, {9,111, 10,11,12}, {13,14,222, 15,16}, {23, 56, 34, 67, 87}};
+            //int[,] output = new int[5, 5] { {13,9,5,1},{14,10,6,2},{15,11,7,3},{16,12,8,4}};
+            LaunchPad.Puzzles.Rotate2DMatrix(input, 5);
+            //bool flag = Enumerable.SequenceEqual(input, output);
+        }
     }
 }
