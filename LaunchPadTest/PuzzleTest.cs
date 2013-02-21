df7bf7b94b06dc188a390c236349c709845d75a5
anurag verma <anuragverma29@gmail.com>
2013-02-21 18:12:23 +0530

    code for puzzle to set row and column of a matrix to zero
    code for an algorithm such that if an element in an MxN matrix is 0, its
    entire row and column is set to 0.

--- LaunchPadTest/PuzzleTest.cs
+++ LaunchPadTest/PuzzleTest.cs
@@ -52,12 +52,21 @@ namespace LaunchPadTest
         }
 
         [TestMethod]
+        //Incomplete .. need to fix this. Enumerable.SequenceEqual can't compare multidimensional arrays
         public void Rotate2DMatrixTest()
         {
             int[,] input = new int[5,5] { {1,2,3,4,9}, {5,6,198,7,8}, {9,111, 10,11,12}, {13,14,222, 15,16}, {23, 56, 34, 67, 87}};
             //int[,] output = new int[5, 5] { {13,9,5,1},{14,10,6,2},{15,11,7,3},{16,12,8,4}};
             LaunchPad.Puzzles.Rotate2DMatrix(input, 5);
             //bool flag = Enumerable.SequenceEqual(input, output);
+        }
+
+        [TestMethod]
+        //Incomplete .. need to fix this. Enumerable.SequenceEqual can't compare multidimensional arrays
+        public void SetZerosTest()
+        {
+            int[,] input = new int[4, 4] { { 1, 2, 0, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 }, };
+            LaunchPad.Puzzles.SetZeros(input);
         }
     }
 }
