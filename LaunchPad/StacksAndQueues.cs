a7219a2b08472e926e81c1e3c08dfe47f21c1120
anurag verma <anuragverma29@gmail.com>
2013-02-25 18:16:51 +0530

    added code to implement 3 stacks from a one array

--- LaunchPad/StacksAndQueues.cs
+++ LaunchPad/StacksAndQueues.cs
@@ -8,9 +8,57 @@ namespace LaunchPad
 {
     class StacksAndQueues
     {
-        #region Describe how you could use a single array to implement three stacks
-        #endregion
+        
     }
+
+    #region Describe how you could use a single array to implement three stacks
+    class ThreeStacksFromOneArray
+    {
+        /*
+         * Divide the array in three equal parts and allow the individual stack to grow in that limited space
+         * (note: “[“ means inclusive, while “(“ means exclusive of the end point).
+         * for stack 1, we will use [0, n/3)
+         * for stack 2, we will use [n/3, 2n/3)
+         * for stack 3, we will use [2n/3, n)
+         * This solution is based on the assumption that we do not have any extra information 
+         * about the usage of space by individual stacks and that we can’t either modify or use any extra space. 
+         * With these constraints, we are left with no other choice but to divide equally.
+         */
+        static int stackSize = 300;
+        int[] buffer = new int[stackSize * 3];
+        int[] stackPointer = { 0, 0, 0}; //stack pointers to track top elements
+
+        //stackNum is the stack in which value needs to be inserted
+        void Push(int stackNum, int value)
+        {
+            //find index at which the insertion should be made
+            int index = stackNum * stackSize + stackPointer[stackNum] + 1;
+            stackPointer[stackNum]++;
+            buffer[index] = value;
+        }
+
+        int Pop(int stackNum)
+        {
+            int index = stackNum * stackSize + stackPointer[stackNum];
+            stackPointer[stackNum]--;
+            int value = buffer[index];
+            //set the value of the popped index as 0
+            buffer[index] = 0;
+            return value;
+        }
+
+        int Peek(int stackNum)
+        {
+            int index = stackNum * stackSize + stackPointer[stackNum];
+            return buffer[index];
+        }
+
+        bool IsEmpty(int stackNum)
+        {
+            return (stackPointer[stackNum] == 0);
+        }
+    }
+    #endregion
 
     class Stack
     {
